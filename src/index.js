37e4974152857a5085e51867af41d30ef88dc4ec
Johann <johann.maiga@gmail.com>
2020-09-16 22:22:51 -0400

    scripts updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Get the modal
-var modal = document.getElementById('id01');
-
+var modal = document.getElementById('id01') = function () { 
+location.href = "https://100trivia2ef60f26-2ef60f26-dev.auth.us-east-1.amazoncognito.com/login?client_id=3poh7o7k5v4a9561h8ft02rkaa&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://auth.100trivia.com/";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
